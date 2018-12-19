d94cc0922229151ed8816bf685a532ccfdae288e
Valeriy Svydenko <vsvydenk@redhat.com>
2018-12-19 16:37:11 +0200

    Remove maven server
    Signed-off-by: Valeriy Svydenko <vsvydenk@redhat.com>

--- plugins/plugin-maven/che-plugin-maven-shared/src/main/java/org/eclipse/che/plugin/maven/shared/MavenAttributes.java
+++ plugins/plugin-maven/che-plugin-maven-shared/src/main/java/org/eclipse/che/plugin/maven/shared/MavenAttributes.java
@@ -31,37 +31,18 @@ public interface MavenAttributes {
   String VERSION = "maven.version";
   String PACKAGING = "maven.packaging";
   String PARENT_GROUP_ID = "maven.parent.groupId";
-  String PARENT_ARTIFACT_ID = "maven.parent.artifactId";
   String PARENT_VERSION = "maven.parent.version";
 
   String TEST_SOURCE_FOLDER = "maven.test.source.folder";
-  String RESOURCE_FOLDER = "maven.resource.folder";
 
   String DEFAULT_SOURCE_FOLDER = "src/main/java";
-  String DEFAULT_RESOURCES_FOLDER = "src/main/resources";
   String DEFAULT_TEST_SOURCE_FOLDER = "src/test/java";
-  String DEFAULT_TEST_RESOURCES_FOLDER = "src/test/resources";
   String DEFAULT_VERSION = "1.0-SNAPSHOT";
   String DEFAULT_PACKAGING = "jar";
-  String DEFAULT_OUTPUT_FOLDER = "target/classes";
-
-  String DEFAULT_OUTPUT_DIRECTORY = "target/classes";
-  String DEFAULT_TEST_OUTPUT_DIRECTORY = "target/test-classes";
 
   String POM_XML = "pom.xml";
 
   String UNKNOWN_VALUE = "unknown";
-
-  /** Name of WebSocket chanel */
-  String MAVEN_CHANEL_NAME = "maven:workspace";
-
-  String MAVEN_OUTPUT_UNSUBSCRIBE = "mavenOutput/unsubscribe";
-  String MAVEN_OUTPUT_SUBSCRIBE = "mavenOutput/subscribe";
-  String MAVEN_OUTPUT_TEXT_METHOD = "mavenOutput/text";
-  String MAVEN_OUTPUT_UPDATE_METHOD = "mavenOutput/update";
-  String MAVEN_OUTPUT_START_STOP_METHOD = "mavenOutput/start_stop";
-  String MAVEN_OUTPUT_PERCENT_METHOD = "mavenOutput/percent";
-  String MAVEN_OUTPUT_PERCENT_UNDEFINED_METHOD = "mavenOutput/percentUndefined";
 
   String MAVEN_ARCHETYPE_CHANEL_UNSUBSCRIBE = "mavenArchetype/unsubscribe";
   String MAVEN_ARCHETYPE_CHANEL_SUBSCRIBE = "mavenArchetype/subscribe";
